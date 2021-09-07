5e6ff802dc9493f0f7d6addfa8c86a10e7875a75
Adrian Camacho <adriancamacho18@gmail.com>
2021-09-07 14:58:11 -0700

    add buttons to switch back and forth from components

---
+++ src/components/NewTicketForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewTicketForm(props){
+  return (
+    <>
+      <h3>This is a form.</h3>
+    </>
+  );
+}
+
+export default NewTicketForm;
---
+++ src/components/TicketControl.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import NewTicketForm from './NewTicketForm';
+import TicketList from './TicketList';
+
+class TicketControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState(prevState => 
+      ({formVisibleOnPage: !prevState.formVisibleOnPage}));
+  }
+
+  render() {
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTicketForm />
+      buttonText = 'Return to Ticket List';
+    } else {
+      currentlyVisibleState = <TicketList />
+      buttonText = 'Add Ticket';
+    }
+    return (
+      <>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
+      </>
+    )
+  }
+}
+
+export default TicketControl;
