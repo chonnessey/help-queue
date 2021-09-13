28dbb89400237be096f00002675106e45adddb8f
Adrian Camacho <adriancamacho18@gmail.com>
2021-09-13 10:17:17 -0700

    add reusable form

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ReusableForm(props) {
+  return (
+    <>
+      <form onSubmit={props.formSubmissionHandler}>
+        <input
+          type='text'
+          name='names'
+          placeholder='Pair Names' />
+        <input
+          type='text'
+          name='location'
+          placeholder='Location' />
+        <textarea
+          name='issue'
+          placeholder='Describe your issue.' />
+        <button type='submit'>{props.buttonText}</button>
+      </form>
+    </>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
